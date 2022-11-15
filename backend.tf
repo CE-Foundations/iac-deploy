6bb81d38e169c4a6b391f595486c376c005808ac
Paul Durivage <1240740+angstwad@users.noreply.github.com>
2022-11-15 13:42:45 -0700

    format files

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket  = "inf-mcdtest-state-743"
-    prefix  = "terraform/state"
+    bucket = "inf-mcdtest-state-743"
+    prefix = "terraform/state"
   }
 }
 
