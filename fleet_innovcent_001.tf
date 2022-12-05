86add0631ae73286380edb1b2ecc9eb3f152ee60
Paul Durivage <1240740+angstwad@users.noreply.github.com>
2022-12-05 14:43:39 -0700

    update fleet org module

--- fleet_innovcent_001.tf
+++ fleet_innovcent_001.tf
@@ -1,8 +1,10 @@
 // An example fleet deployment
-module "fleet_innovcent_001" {
-  source             = "github.com/secretspecialsauce/iac-fleet-mod"
-  org_id             = var.org_id
-  fleet_folder_name  = "fleet-innovcent-001"
-  region_folder_name = google_folder.usa.name
-  billing_account_id = var.billing_account_id
+module "fleet_innovcent_org" {
+  source = "github.com/secretspecialsauce/iac-fleet-mod//fleet_org"
+
+  org_id              = var.org_id
+  project_prefix      = var.project_prefix
+  fleet_name          = "innovcent"
+  fleet_parent_folder = google_folder.usa.name
+  billing_account_id  = var.billing_account_id
 }
