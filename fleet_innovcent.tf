2efe2d0235736ec4283b2c56f9dc61bfdd5c351a
Paul Durivage <1240740+angstwad@users.noreply.github.com>
2022-12-06 11:03:14 -0700

    use service accounts module

--- fleet_innovcent.tf
+++ fleet_innovcent.tf
@@ -8,3 +8,10 @@ module "fleet_innovcent_org" {
   fleet_parent_folder = google_folder.usa.name
   billing_account_id  = var.billing_account_id
 }
+
+module "fleet_innovcent_sa_cluster_1" {
+  source = "github.com/secretspecialsauce/iac-fleet-mod//cluster_service_accounts"
+
+  cluster_name = "clus-1"
+  project_id   = module.fleet_innovcent_org.control_plane_service_account_project.project_id
+}
