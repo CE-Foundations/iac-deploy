e34f5593d83d79f309c056d8b10d429876742cec
Paul Durivage <1240740+angstwad@users.noreply.github.com>
2022-12-14 15:59:09 -0700

    update tf fmt

--- 1-fleet/fleet_poc.tf
+++ 1-fleet/fleet_poc.tf
@@ -76,7 +76,7 @@ module "fleet_poc_cluster_0" {
   gsa_gateway_connect_agent_iam_project  = module.fleet_poc.fleet_project.project_id
   gsa_cdi_import_agent_iam_project       = module.fleet_poc.fleet_project.project_id # TODO assign global control plane
   gsa_storage_agent_iam_project          = module.fleet_poc.fleet_project.project_id # TODO same as SDS
-  
+
   sds_bucket_location = "us-central1"
   sds_project_id      = module.fleet_poc.control_plane_sds_project.project_id
 }
