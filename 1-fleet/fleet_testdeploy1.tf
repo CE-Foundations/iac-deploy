0e2cb8b3d1e7f3943e11b81a5d5ac3000d0faab4
Paul Durivage <durivage@google.com>
2022-12-15 17:12:28 -0700

    update to latest cluster module

--- 1-fleet/fleet_testdeploy1.tf
+++ 1-fleet/fleet_testdeploy1.tf
@@ -47,18 +47,18 @@ module "fleet_testdeploy1_clust-matt-1" {
 
   # GCP project where GSAs are created
   gsa_project_id    = module.fleet_testdeploy1.control_plane_service_account_project.project_id
-  secret_project_id = module.fleet_testdeploy1.control_plane_secret_manager_project.project_id
+  secret_project_id = module.fleet_testdeploy1.control_plane_secrets_project.project_id
   # Projects where IAM bindings are created
   gsa_gcr_agent_iam_project              = module.fleet_testdeploy1.fleet_project.project_id
   gsa_abm_gke_connect_agent_iam_project  = module.fleet_testdeploy1.fleet_project.project_id
   gsa_abm_gke_register_agent_iam_project = module.fleet_testdeploy1.fleet_project.project_id
   gsa_abm_ops_agent_iam_project          = module.fleet_testdeploy1.fleet_project.project_id
   gsa_acm_monitoring_agent_iam_project   = module.fleet_testdeploy1.fleet_project.project_id
-  gsa_external_secrets_iam_project       = module.fleet_testdeploy1.control_plane_secret_manager_project.project_id
-  gsa_sds_backup_agent_iam_project       = module.fleet_testdeploy1.fleet_project.project_id # TODO buckets to be created
+  gsa_external_secrets_iam_project       = module.fleet_testdeploy1.control_plane_secrets_project.project_id
+  gsa_sds_backup_agent_iam_project       = module.fleet_testdeploy1.fleet_project.project_id
   gsa_gateway_connect_agent_iam_project  = module.fleet_testdeploy1.fleet_project.project_id
-  gsa_cdi_import_agent_iam_project       = module.fleet_testdeploy1.fleet_project.project_id # TODO assign global control plane
-  gsa_storage_agent_iam_project          = module.fleet_testdeploy1.fleet_project.project_id # TODO same as SDS
+  gsa_cdi_import_agent_iam_project       = module.fleet_testdeploy1.fleet_project.project_id
+  gsa_storage_agent_iam_project          = module.fleet_testdeploy1.fleet_project.project_id
 
   sds_bucket_location = "us-central1"
   sds_project_id      = module.fleet_testdeploy1.control_plane_sds_project.project_id
