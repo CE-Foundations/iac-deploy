8f0c13600818a0eba6c4551a15abfcca844fc2c0
Luis Gonzalez <luisegr149@gmail.com>
2023-01-05 01:44:57 -0600

    Updated variables

---
+++ 1-fleet/variables.tf
@@ -0,0 +1,12 @@
+variable "org_id" {
+  description = "The id of the organization"
+  type        = string
+}
+
+variable "billing_account_id" {
+  type = string
+}
+
+variable "project_prefix" {
+  type = string
+}
