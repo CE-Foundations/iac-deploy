34571d68dbf0f28e30625be58a564d36ccd86654
Luis Gonzalez <luisegr149@gmail.com>
2023-01-05 01:25:33 -0600

    Remove control plane temporary

--- 0-baseline/global_control_plane.tf
+++ 0-baseline/global_control_plane.tf
@@ -1,53 +1,53 @@
-# Implements "global control plane" modules to create org, folder, project structure
-module "top_level_org" {
-  source = "github.com/secretspecialsauce/iac-global-control-plane-mod//top_level_org"
-  org_id = var.org_id
-}
+# # Implements "global control plane" modules to create org, folder, project structure
+# module "top_level_org" {
+#   source = "github.com/secretspecialsauce/iac-global-control-plane-mod//top_level_org"
+#   org_id = var.org_id
+# }
 
-module "global_control_plane_billing_org" {
-  source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//billing_org"
-  org_id                           = var.org_id
-  billing_account_id               = var.billing_account_id
-  global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
-  project_prefix                   = var.project_prefix
-}
-
-module "global_control_plane_automation_org" {
-  source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//automation_resources"
-  org_id                           = var.org_id
-  billing_account_id               = var.billing_account_id
-  global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
-  project_prefix                   = var.project_prefix
-}
-
-module "global_control_plane_common_services_org" {
-  source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//common_services_org"
-  org_id                           = var.org_id
-  billing_account_id               = var.billing_account_id
-  global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
-  project_prefix                   = var.project_prefix
-}
-
-module "global_control_plane_network_org" {
-  source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//network_org"
-  org_id                           = var.org_id
-  billing_account_id               = var.billing_account_id
-  global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
-  project_prefix                   = var.project_prefix
-}
-
-module "global_control_plane_security_org" {
-  source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//security_org"
-  org_id                           = var.org_id
-  billing_account_id               = var.billing_account_id
-  global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
-  project_prefix                   = var.project_prefix
-}
-
-# module "global_control_plane_logging_org" {
-#   source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//logging_org"
+# module "global_control_plane_billing_org" {
+#   source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//billing_org"
 #   org_id                           = var.org_id
 #   billing_account_id               = var.billing_account_id
 #   global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
 #   project_prefix                   = var.project_prefix
 # }
+
+# module "global_control_plane_automation_org" {
+#   source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//automation_resources"
+#   org_id                           = var.org_id
+#   billing_account_id               = var.billing_account_id
+#   global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
+#   project_prefix                   = var.project_prefix
+# }
+
+# module "global_control_plane_common_services_org" {
+#   source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//common_services_org"
+#   org_id                           = var.org_id
+#   billing_account_id               = var.billing_account_id
+#   global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
+#   project_prefix                   = var.project_prefix
+# }
+
+# module "global_control_plane_network_org" {
+#   source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//network_org"
+#   org_id                           = var.org_id
+#   billing_account_id               = var.billing_account_id
+#   global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
+#   project_prefix                   = var.project_prefix
+# }
+
+# module "global_control_plane_security_org" {
+#   source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//security_org"
+#   org_id                           = var.org_id
+#   billing_account_id               = var.billing_account_id
+#   global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
+#   project_prefix                   = var.project_prefix
+# }
+
+# # module "global_control_plane_logging_org" {
+# #   source                           = "github.com/secretspecialsauce/iac-global-control-plane-mod//logging_org"
+# #   org_id                           = var.org_id
+# #   billing_account_id               = var.billing_account_id
+# #   global_control_plane_folder_name = module.top_level_org.global_control_plane_folder_name
+# #   project_prefix                   = var.project_prefix
+# # }
