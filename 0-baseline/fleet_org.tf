4814ee62884ac459c1f5811a1de8d2a3342fe271
Luis Gonzalez <luisegr149@gmail.com>
2023-01-04 23:51:59 -0600

    Deleted not required variables

--- 0-baseline/fleet_org.tf
+++ 0-baseline/fleet_org.tf
@@ -2,7 +2,5 @@ module "fleet_org" {
   source                           = "github.com/CE-Foundations/iac-fleet-org-mod"
   org_id                           = var.org_id
   org                              = var.org
-  billing_account_id               = var.billing_account_id
-  project_prefix                   = var.project_prefix
   parent_folder                    = var.parent_folder
 }
